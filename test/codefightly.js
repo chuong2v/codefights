1919139a3c68d67942e2d809653ed001f3779851
chuong2v <chuong.2v@gmail.com>
2016-09-09 14:04:43 +0700

    add test case

---
+++ test/codefightly.js
@@ -0,0 +1,74 @@
+Input:
+
+alphabet: "abcdefg"
+code: "bbbb"
+
+Expected Output:
+
+"bbbc"
+
+
+
+Input:
+
+alphabet: "a1b2c3"
+code: "b333"
+
+Expected Output:
+
+"2aaa"
+
+
+
+Input:
+
+alphabet: ".Calvin"
+code: "nnnnn"
+
+Expected Output:
+
+"C....."
+
+
+
+Input:
+
+alphabet: "AHBCDEFGIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~"
+code: "~~~~~~~~~~"
+
+Expected Output:
+
+"HAAAAAAAAAA"
+
+
+Input:
+
+alphabet: "01"
+code: "1111011111"
+
+Expected Output:
+
+"1111100000"
+
+
+
+Input:
+
+alphabet: "codefightlys_map."
+code: "a_code."
+
+Expected Output:
+
+"a_codfc"
+
+
+
+Input:
+
+alphabet: "rlamoni"
+code: "r"
+
+Expected Output:
+
+"l"
+
